e053b82eda721ee7166c3939a880460cef25a9ee
Albert Lin <albertkingdom@gmail.com>
2020-04-16 00:18:59 +0800

    update _connect_db.js to connect cleardb

--- _connect_db.js
+++ _connect_db.js
@@ -2,11 +2,14 @@ const mysql = require("mysql");
 const bluebird = require("bluebird");
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "todolist",
-
+  // host: "localhost",
+  // user: "root",
+  // password: "",
+  // database: "todolist",
+  host: "us-cdbr-iron-east-01.cleardb.net",
+  user: "bad362786345d6",
+  password: "00c6a2d7",
+  database: "heroku_2a125441f8e62f0",
   // for MAC PC USE
   // socketPath: "/Applications/XAMPP/xamppfiles/var/mysql/mysql.sock"
 });
