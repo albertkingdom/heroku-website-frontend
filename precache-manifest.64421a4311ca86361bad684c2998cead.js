be83a96b6cb73eb528a3a4019cd808cf38653d87
albert <albertkingdom@gmail.com>
2020-07-29 16:54:50 +0800

    Updates

---
+++ precache-manifest.64421a4311ca86361bad684c2998cead.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf833069e895bafedd8eb94823fbfcb5",
+    "url": "/heroku-website-frontend/index.html"
+  },
+  {
+    "revision": "0b876436afc9ac343ec1",
+    "url": "/heroku-website-frontend/static/css/2.c8fdb744.chunk.css"
+  },
+  {
+    "revision": "783facc80c6305991d38",
+    "url": "/heroku-website-frontend/static/css/main.3d4ca2b3.chunk.css"
+  },
+  {
+    "revision": "0b876436afc9ac343ec1",
+    "url": "/heroku-website-frontend/static/js/2.83bd8c05.chunk.js"
+  },
+  {
+    "revision": "68857271d3c9a8a945c1378722557246",
+    "url": "/heroku-website-frontend/static/js/2.83bd8c05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "783facc80c6305991d38",
+    "url": "/heroku-website-frontend/static/js/main.7c294c3c.chunk.js"
+  },
+  {
+    "revision": "724655e7654a4ca13307",
+    "url": "/heroku-website-frontend/static/js/runtime-main.12390857.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/heroku-website-frontend/static/media/logo.5d5d9eef.svg"
+  }
+]);
